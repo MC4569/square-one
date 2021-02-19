bc681ed940de31f611cb365e89622e421766abd8
MC4569 <mcollins891@gmail.com>
2021-02-19 16:13:52 -0500

    signInForm commit

--- client/src/components/authentication/SignInForm.js
+++ client/src/components/authentication/SignInForm.js
@@ -29,29 +29,31 @@ const SignInForm = () => {
     setErrors(newErrors);
   };
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
     validateInput(userPayload);
-    if (Object.keys(errors).length === 0) {
-      fetch("/api/v1/user-sessions", {
-        method: "post",
-        body: JSON.stringify(userPayload),
-        headers: new Headers({
-          "Content-Type": "application/json",
-        }),
-      }).then((resp) => {
-        if (resp.ok) {
-          resp.json().then(() => {
-            setShouldRedirect(true);
-          });
-        } else {
-          const errorMessage = `${resp.status} (${resp.statusText})`;
-          const error = new Error(errorMessage);
-          throw error;
-        }
-      });
+    try {
+      if (Object.keys(errors).length === 0) {
+        const response = await fetch("/api/v1/user-sessions", {
+          method: "post",
+          body: JSON.stringify(userPayload),
+          headers: new Headers({
+            "Content-Type": "application/json",
+          }),
+        })
+        
+        if (!response.ok) {
+          const errorMessage = `${response.status} (${response.statusText})`;
+          throw new Error(errorMessage);
+        } 
+        
+        setShouldRedirect(true)
+      }
+    } catch (error) {
+      console.error(`Error in Fetch: ${error.message}`)
     }
   };
+  
   const onInputChange = (event) => {
     setUserPayload({
       ...userPayload,
