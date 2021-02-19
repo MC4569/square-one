6db1803bc685693ddd6e3fa97cade2bdb59fe88d
MC4569 <mcollins891@gmail.com>
2021-02-18 22:08:12 -0500

    review form commit

--- client/src/components/ReviewList.js
+++ client/src/components/ReviewList.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react'
 
 import ReviewTile from './ReviewTile'
 
-const ReviewList = ({reviews}) => {
+const ReviewList = ({reviews, user, errors}) => {
   const reviewTiles = reviews.map(review => {
     return (
       <ReviewTile
         key={review.id}
         review={review} 
+        user={user}
+        errors={errors}
       />
     )
   })
